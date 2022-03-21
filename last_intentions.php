ed01573d4be3f324539804dbfffe664e8ea05c8a
Kamil Wozniak <kamil@tuware.com>
2022-03-21 19:13:23 +0000

    2022-03-20

--- last_intentions.php
+++ last_intentions.php
@@ -1,91 +1,79 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZALNE<br />
-            14.03. – 20.03.2022&nbsp;r.
+            21.03. – 27.03.2022&nbsp;r.
         </h3>
     </div>
     <div class="col">
-<p class="intentions_date">14&nbsp;marca, Poniedziałek</p>
+<p class="intentions_date">21&nbsp;marca, Poniedziałek</p>
 <ul>
-    <li>6.30 + Andrzeja w&nbsp;11&nbsp;r.&nbsp;śm., Józefę, Idziego
-    Dudków</li>
-    <li>6.30 + Barbarę Bryłę</li>
-    <li>18.00 + Mariannę Kozakiewicz</li>
-    <li>18.00 + Janinę Wójcik</li>
+<li>6.30 + Mariannę Kozakiewicz</li>
+<li>6.30 + Jadwigę w&nbsp;r. &nbsp;śm., Szczepana Kasprzyckich,
+zm.&nbsp;z&nbsp;rodz. oraz dusze w&nbsp;czyśćcu cierpiące</li>
+<li>18.00 + Mariana Mroczkowskiego</li>
+<li>18.00 + Mariana Marczyka w&nbsp;8&nbsp;r.&nbsp;śm.
+i&nbsp;zm.&nbsp;z&nbsp;rodz. Marczyków</li>
 </ul>
 
-<p class="intentions_date">15&nbsp;marca, Wtorek</p>
+<p class="intentions_date">22&nbsp;marca, Wtorek</p>
 <ul>
-    <li>6.30 + Urszule Obitko</li>
-    <li>6.30 + Stanisławę, Władysława Bokwów</li>
-    <li>18.00 w&nbsp;int. osób uzależnionych i&nbsp;ich rodzin,
-    o&nbsp;Boże błogosławieństwo, łaskę przemiany, uzdrowienia
-    i&nbsp;wyzwolenia</li>
-    <li>18.00 o&nbsp;Boże błogosławieństwo, dary Ducha Świętego,
-    opiekę Matki Bożej, potrzebne łaski szczególnie łaskę zdrowia dla
-    Joanny i&nbsp;Andrzeja Skoczkowskich</li>
-    <li>18.00 + Ryszarda Chrząstowskiego</li>
+<li>6.30 + Jana Chmiela</li>
+<li>6.30 + Adama Kwietniewskiego</li>
+<li>18.00 + Mariannę, Szymona, Zbigniewa, Mariana Woźniaków;
+Agnieszkę, Józefa Adamczyków</li>
+<li>18.00 + Stanisławę Gowin w&nbsp;1&nbsp;r.&nbsp;śm.</li>
 </ul>
 
-<p class="intentions_date">16&nbsp;marca, Środa</p>
+<p class="intentions_date">23&nbsp;marca, Środa</p>
 <ul>
-    <li>6.30 o&nbsp;Boże błogosławieństwo, dary Ducha Świętego,
-    opiekę Matki Bożej, łaskę wiary i&nbsp;zdrowie dla Pawła</li>
-    <li>6.30 + Kazimierza Płazę; Stanisławę, Józefa, Jana zm.
-    z&nbsp;rodz. i&nbsp;dusze w&nbsp;czyśćcu cierpiące</li>
-    <li>18.00 + Zdzisława Romanowskiego w&nbsp;30&nbsp;dz.
-    po&nbsp;śm.</li>
-    <li>18.00 + Marię Włoch</li>
-    <li>18.00 + Zygmunta Mazura</li>
+<li>6.30 + Adama Nagórnego</li>
+<li>6.30 + Urszulę Obitko</li>
+<li>18.00 + Mariannę, Tadeusza, Marka Niemczyków</li>
+<li>18.00 + Marka Lipca</li>
+<li>18.00 + Teresę Wandę Łukaszek w&nbsp;2&nbsp;r.&nbsp;śm.</li>
 </ul>
 
-<p class="intentions_date">17&nbsp;marca, Czwartek</p>
+<p class="intentions_date">24&nbsp;marca, Czwartek</p>
 <ul>
-    <li>6.30 + Zbigniewa Gądka</li>
-    <li>6.30 + Halinę Orłowską</li>
-    <li>18.00 + Weronikę Szpatowicz w&nbsp;30&nbsp;dz.
-    po&nbsp;śm.</li>
-    <li>18.00 + Ryszarda Chrząstowskiego</li>
-    <li>18.00 + Tadeusza Śledzia</li>
+<li>6.30 + Barbarę Bryłę; Jacka Lipca w&nbsp;2&nbsp;r.&nbsp;śm.</li>
+<li>6.30 + Urszulę Obitko</li>
+<li>18.00 + Janinę Wójcik</li>
+<li>18.00 + Zofię Chmielewską</li>
 </ul>
 
-<p class="intentions_date">18&nbsp;marca, Piątek</p>
+<p class="intentions_date">25&nbsp;marca, Piątek –
+Uroczystość Zwiastowania Pańskiego</p>
 <ul>
-    <li>6.30 w&nbsp;int. dzieci i&nbsp;chrześniaków oraz ich rodzin
-    o&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę Matki
-    Bożej</li>
-    <li>6.30 + Jana w&nbsp;7&nbsp;r.&nbsp;śm. oraz jego rodziców</li>
-    <li>18.00 dziękczynno-błagalna w&nbsp;int. mieszkańców bloku
-    ul.&nbsp;Maciejowskiego 11, klatka&nbsp;4</li>
-    <li>18.00+ Józefę, Jana Marców; Ryszarda Pietruszkę</li>
-    <li>18.00 + Jerzego Malca</li>
+<li>6.30 dziękczynno-błagalna o&nbsp;Boże błogosławieństwo, dary
+Ducha Świętego, opiekę Matki Bożej, potrzebne łaski dla Grażyny
+i&nbsp;Zbigniewa z&nbsp;racji 70&nbsp;r.&nbsp;urodzin</li>
+<li>9.00 + Janinę Wójcik</li>
+<li>18.00 dziękczynno-błagalna o&nbsp;Boże Miłosierdzie, dar wiary,
+nadziei i&nbsp;miłości, łaskę pokory, nawrócenia i&nbsp;uzdrowienia
+w&nbsp;rodzinie</li>
+<li>18.00+ Lucję Lis</li>
 </ul>
 
-<p class="intentions_date">19&nbsp;marca, Sobota –
-Uroczystość św.&nbsp;Józefa, Oblubieńca NMP</p>
+<p class="intentions_date">26&nbsp;marca, Sobota</p>
 <ul>
-    <li>6.30 + Helenę, Jerzego Bażantów</li>
-    <li>6.30 + w&nbsp;int. zmarłych z&nbsp;rodz. Danuty
-    i&nbsp;Ryszarda</li>
-    <li>18.00 w&nbsp;int. Bogusława z&nbsp;racji urodzin
-    i&nbsp;imienin o&nbsp;Boże błogosławieństwo, opiekę Matki Bożej,
-    potrzebne łaski przez wstawiennictwo św.&nbsp;Józefa Opiekuna
-    rodzin</li>
-    <li>18.00 + Henrykę Figas</li>
+<li>6.30 + Wojciecha Piskóra</li>
+<li>6.30 + Adama Kwietniewskiego</li>
+<li>18.00 + Jana, Józefa Ochmeraków i&nbsp;zm.&nbsp;z&nbsp;rodz.</li>
+<li>18.00 + Jana Skorupskiego w&nbsp;28&nbsp;r.&nbsp;śm.</li>
 </ul>
 
-<p class="intentions_date">20&nbsp;marca, Niedziela –
+<p class="intentions_date">27&nbsp;marca, Niedziela –
     <span style="color: grey">
-        III&nbsp;Niedziela Wielkiego Postu
+        IV&nbsp;Niedziela Wielkiego Postu
     </span>
 </p>
 <ul>
-    <li>7.30 + Barbarę Pyszczyńską w&nbsp;8&nbsp;r.&nbsp;śm.</li>
-    <li>9.30 + Dorotę Kwitek i&nbsp;zm. z&nbsp;rodz. Stefańskich
-    i&nbsp;Wronów</li>
-    <li>11.15 w&nbsp;int. Kręgu Domowego Kościoła</li>
-    <li>12.30 w&nbsp;int. parafian</li>
-    <li>18.00 + Józefa Zubę; Wacława, Mariannę Niziałków</li>
+<li>7.30 + Mariannę, Romana, Antoninę, Józefa, Marcina Ozdobów</li>
+<li>9.30 + Tadeusza Lipczyńskiego</li>
+<li>11.15 w&nbsp;int. parafian</li>
+<li>12.30 + Alicję Gilewską; Józefa, Stefanię Janus; Krystynę
+Polit</li>
+<li>18.00 + Stanisława, Janinę Zimnickich</li>
 </ul>
     </div>
 </div>
--- last_post.php
+++ last_post.php
@@ -4,7 +4,61 @@
         <h3>III Niedziela Wielkiego Postu, Rok C</h3>
     </div>
     <div class="col">
-
+        <p>1. Słowa wdzięczności kierujemy do&nbsp;parafian za&nbsp;złożone
+        ofiary na&nbsp;rzecz uchodźców z&nbsp;Ukrainy, na&nbsp;wyposażenie
+        mieszkania, w&nbsp;którym przebywają, w&nbsp;budynku obok plebanii,
+        tj.&nbsp;5126&nbsp;zł i&nbsp;na&nbsp;rzecz misji 1989&nbsp;zł.
+        Dziękujemy za&nbsp;dary w&nbsp;postaci żywności, środków czystości
+        i&nbsp;opatrunków. Serdeczne <em>Bóg zapłać</em>.</p>
+        <p>2. W&nbsp;przyszłą niedzielę wizytacja kanoniczna w&nbsp;naszej
+        parafii. Ks.&nbsp;Biskup Ordynariusz spotka się z&nbsp;Radą
+        Parafialną na&nbsp;plebanii o&nbsp;godz.&nbsp;11.00, potem
+        w&nbsp;kościele o&nbsp;godz.&nbsp;12.00, z&nbsp;poszczególnymi
+        grupami duszpasterskim: Legionem Maryi, z&nbsp;wszystkimi Różami
+        Różańcowymi z&nbsp;całej parafii, z&nbsp;Kościołem Domowym,
+        z&nbsp;Rodziną Szkaplerzną, Służbą Ołtarza i&nbsp;innymi.
+        O&nbsp;12.30 - Msza&nbsp;św. w&nbsp;intencji naszej parafii.
+        Zapraszamy.</p>
+        <p>3. Serdecznie zapraszamy wszystkich zainteresowanych
+        na&nbsp;wykłady otwarte z&nbsp;filozofii. Wykład odbędzie się
+        w&nbsp;sobotę, tj.&nbsp;26&nbsp;marca br. o&nbsp;godz.&nbsp;16.00
+        w&nbsp;Sali Papieskiej Instytutu Teologicznego, ul.&nbsp;Mariacka 9.
+        Temat wykładu: O&nbsp;wojnie sprawiedliwej i&nbsp;niesprawiedliwej.
+        (prowadzący ks.&nbsp;dr&nbsp;Bartłomiej Krzos).</p>
+        <p>4. W&nbsp;Uroczystość Zwiastowania Pańskiego, 25&nbsp;marca,
+        kościół w&nbsp;Polsce obchodzi dzień Świętości Życia. Jego celem jest
+        budzenie wrażliwości na&nbsp;sens i&nbsp;wartość ludzkiego życia,
+        a&nbsp;każdym jego etapie oraz zwrócenie uwagi na&nbsp;potrzebę
+        szczególnej troski o&nbsp;nie. Wyrazem takiej troski jest m.in.
+        podjęcie Duchowej Adopcji dziecka Poczętego, czyli zobowiązanie się
+        do&nbsp;modlitwy w&nbsp;intencji dziecka zagrożonego zabiciem
+        w&nbsp;łonie matki. Zachęcamy do&nbsp;włączenia się
+        w&nbsp;tę&nbsp;inicjatywę. Deklaracje Duchowej Adopcji w&nbsp;formie
+        małych książeczek znajdują się na&nbsp;stoliku obok prasy
+        katolickiej. Uroczyste złożenie przyrzeczenia, które zapoczątkuje
+        duchową Adopcję, odbędzie się na&nbsp;Mszy&nbsp;św.
+        o&nbsp;godz.&nbsp;18.00 w&nbsp;naszym kościele parafialnym
+        25&nbsp;marca, tj.&nbsp;w&nbsp;najbliższy piątek. Msze&nbsp;św.
+        w&nbsp;tym dniu o&nbsp;godz.&nbsp;6.30 , 9.00 i&nbsp;o&nbsp;18.00.</p>
+        <p>5. W&nbsp;związku z&nbsp;trwającą agresją zbrojną w&nbsp;Ukrainie,
+        dnia 25&nbsp;marca&nbsp;br., w&nbsp;Uroczystość Zwiastowania
+        Pańskiego, Ojciec Święty Franciszek dokona Aktu Poświęcenia Rosji
+        i&nbsp;Ukrainy Niepokalanemu Sercu Maryi. Jednocześnie Papież prosi,
+        aby pasterze i&nbsp;wierni na&nbsp;całym świecie trwali w&nbsp;tym
+        samym czasie zjednoczeni na&nbsp;modlitwie, pod przewodnictwem
+        Biskupa Rzymu. W&nbsp;związku z&nbsp;powyższym w&nbsp;naszym kościele
+        o&nbsp;wspomnianej godzinie wznosząc modlitwy do&nbsp;Boga, odmówimy
+        Akt Poświęcenia Rosji i&nbsp;Ukrainy. Potem Droga Krzyżowa, a&nbsp;po
+        Niej Msza&nbsp;św. w&nbsp;intencji pokoju na&nbsp;Ukrainie
+        i&nbsp;nawrócenia Rosji. Zatem zapraszamy w&nbsp;najbliższy piątek
+        na&nbsp;godzinę 17.00 do&nbsp;naszego kościoła.</p>
+        <p>6. Do&nbsp;nabycia w&nbsp;zakrystii Baranki wielkanocne
+        w&nbsp;cenie 8&nbsp;zł.</p>
+        <p>7. Zachęcamy do&nbsp;nabycia i&nbsp;lektury katolickiej prasy.
+        Gazety na&nbsp;stoliku pod chórem.</p>
+        <p>8. Dziękujemy za&nbsp;modlitwę, wszelką pomoc, składane ofiary
+        na&nbsp;potrzeby parafii i&nbsp;nie tylko. Wszystkim uczestniczącym
+        we&nbsp;Mszy&nbsp;św. życzymy błogosławionej niedzieli.</p>
         <p>
             <span style="float: left;">Sandomierz, 20.&nbsp;marca 2022&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Stanisław Chmielewski</span>
